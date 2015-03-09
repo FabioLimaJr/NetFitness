9a4b7cd693f692c3954ccb00f036521221a6bf61
Fábio <Fbio@Fbio-PC>
2015-03-09 19:34:09 -0300

    Funcionalidade Listar Secretária Repositório/Controlador, Feita por Fábio de Lima

--- controlador/ControladorSecretaria.php
+++ controlador/ControladorSecretaria.php
@@ -1,11 +1,4 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of ControladorSecretaria
  *
@@ -94,9 +87,11 @@ class ControladorSecretaria {
         
     }
     
-    public function excluir($secretaria){
-        
-        $this->repositorioSecretaria->excluir($secretaria);
-        
+    public function excluir($secretaria){ 
+        $this->repositorioSecretaria->excluir($secretaria);    
+    }
+    
+    public function listar(){
+        return $this->getRepositorioSecretaria()->listar();
     }
 }
