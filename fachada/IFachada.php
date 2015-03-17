0d7cb07b97aed823750debde37119022a361ecef
Fábio <Fbio@Fbio-PC>
2015-03-17 19:44:42 -0300

    Merge origin/master
    Conflicts:
    	conexao/Conexao.php
    	fachada/Fachada.php
    	fachada/IFachada.php
    	serverPath.php

--- fachada/IFachada.php
+++ fachada/IFachada.php
@@ -27,38 +27,7 @@ interface IFachada {
     public function logarSecretaria($secretaria);
     public function logarAluno($aluno);
     public function logarInstrutor($instrutor);
-    
     public function logarNutricionista($nutricionista);    
-}
-<?php
-/**
- *
- * @author Marcelo
- */
-interface IFachada {
-    
-    public function incluirAluno($aluno);
-    public function alterarAluno($aluno);    
-    public function excluirAluno($aluno);
-    public function listarAlunos();
-    public function detalharAluno($aluno);
-    
-    public function incluirInstrutor($instrutor);
-    public function alterarInstrutor($instrutor);
-    public function excluirInstrutor($instrutor);
-    public function listarInstrutores();
-    public function detalharInstrutor($instrutor);
-    
-    public function incluirSecretaria($secretaria);
-    public function alterarSecretaria($secretaria);
-    public function excluirSecretaria($secretaria);
-    public function listarSecretaria();
-    public function detalharSecretaria($secretaria);
-    
-    public function logarCoordenador($coordenador);
-    public function logarSecretaria($secretaria);
-    public function logarAluno($aluno);
-    public function logarInstrutor($instrutor);
     
     public function incluirTreino($treino);
     public function alterarTreino($treino);
