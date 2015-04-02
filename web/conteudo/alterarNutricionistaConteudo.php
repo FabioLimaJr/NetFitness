056c9dbd6b8b0d7229a67b7ad17d9127ecaf06c7
Erick Ferreira <efsgarbage@gmail.com>
2015-04-02 15:51:39 -0300

    ajustes Crud e tela Nutricionista

--- web/conteudo/alterarNutricionistaConteudo.php
+++ web/conteudo/alterarNutricionistaConteudo.php
@@ -13,24 +13,23 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 try
 {
     $listaNutricionistas = $fachada->listarNutricionistas();
+    $_SESSION['listaNutricionistas'] = $listaNutricionistas;
 } 
 catch (Exception $exc)
 {
     $mensagem = $exc->getMessage();
 }   
-    
-
 
 ?>
 
 <h1 class="title">Alterar Nutricionistas</h1>
     <div class="line"></div>
     
-        Telefone:<?php //echo $coordenador->getTelefone() ?> | Email:<?php //echo $coordenador->getEmail() ?> | Endereço:<?php //echo $coordenador->getEndereco() ?>
+    Telefone:<?php echo $coordenador->getTelefone() ?> | Email:<?php echo $coordenador->getEmail() ?> | Endereço:<?php echo $coordenador->getEndereco() ?>
 
     <div class="intro" style="margin-bottom:50px"></div>
     
-    <h3>Usuário logado: <?php //echo $coordenador->getNome() ?></h3>
+    <h3>Usuário logado: <?php echo $coordenador->getNome() ?></h3>
    
     <div class="clear"></div>
     <div class="line"></div>
@@ -40,26 +39,30 @@ catch (Exception $exc)
      { ?>
 
     <div class="form-container" style="margin-bottom:50px">
-      <form class="forms" action="alterarDieta.php" method="post" >
+      <form class="forms" action="alterarNutricionista.php" method="post" >
       <fieldset>
       
        <ol>
             <li class="form-row text-input-row">     
                <table style="width:100%;margin-bottom:50px">
                          <tr>
-                           <th>Nome Nutricionista</th> 
+                           <th>Nome</th> 
                            <th>CPF</th>
-                           <th>CRN</th>
+                           <th>Endetreço</th>
                            <th>Telefone</th>
+                           <th>E-mail</th>
+                           <th>CRN</th>
                            <th>Selecionar</th>
                          </tr>
 
                          <?php foreach ($listaNutricionistas as $nutricionista){ ?>
                          <tr>
                              <td><?php echo $nutricionista->getNome() ?></td> 
-                             <td><?php echo $nutricionista->getCpf() ?></td>  
-                             <td><?php echo $nutricionista->getCrn() ?></td>
+                             <td><?php echo $nutricionista->getCpf() ?></td> 
+                              <td><?php echo $nutricionista->getEndereco() ?></td>  
                              <td><?php echo $nutricionista->getTelefone() ?></td>
+                             <td><?php echo $nutricionista->getEmail() ?></td>
+                             <td><?php echo $nutricionista->getCRN() ?></td>
                              <td><input type="radio" name="idNutricionista" value="<?php echo $nutricionista->getIdNutricionista() ?>"></td>
                          </tr>
 
@@ -85,158 +88,99 @@ catch (Exception $exc)
     {
          if(isset($_POST['idNutricionista']))
          {
-             $nutricionista = new Nutricionista($_POST['idNutricionista']);
-             $nutricionistaRetornada = $fachada->detalharDieta($dieta);
-             
-             $listaAlimentosNaoSelecionados = array();
-             $listaAlimentosSelecionados = $dietaRetornada->getListaAlimentos();
-            
-             
-             foreach ($listaAlimentos as $alimento)
+            if($_POST['submit']=="Alterar")
+              {
+            $listaNutricionistaRetornada = $_SESSION['listaNutricionistas'];
+            $nutricionistaSelecionada = null;
+                        
+             foreach ($listaNutricionistaRetornada as $nutricionista)
              {
-                   $alimentoPresente = false;
-                   foreach($listaAlimentosSelecionados as $alimentoSelecionado)
-                   {
-                      
-                       if($alimentoSelecionado->getIdAlimento() == $alimento->getIdAlimento())
-                       {                          
-                           $alimentoPresente = true;
-                       }
-                       
-                   }
-                   if(!$alimentoPresente)
-                   {
-                       array_push($listaAlimentosNaoSelecionados, $alimento);
-                   }
-                 
-                 
+                 if ($nutricionista->getIdNutricionista()==$_POST['idNutricionista']){
+                 $nutricionistaSelecionada = $nutricionista;
+                 break;
+                     }
              }
-             
-         
-             $_SESSION['dietaRetornada']=$dietaRetornada;
-             
+                  
              ?>
     
                 <div class="form-container" style="margin-bottom:50px">
-                    <form class="forms" action="alterarDieta.php" method="post" >
+                    <form class="forms" action="alterarNutricionista.php" method="POST" >
                     <fieldset>
                       <ol>
                          
-                        <li class="form-row text-input-row">
-                            <label style="margin-top:-7px">Aluno</label>                          
-                          <div><?php if(isset($dietaRetornada)) echo $dietaRetornada->getAluno()->getNome()?></div>
-                        </li>  
+                         <li class="form-row text-input-row">
+                        <label>Nome</label>
+                        <input type="text" name="nome" value="<?php 
+                        if(isset($nutricionistaSelecionada)) echo $nutricionistaSelecionada->getNome() ?>" class="text-input" style="white: 300px">
+                    </li>
 
                         <li class="form-row text-input-row">
-                          <label>Descrição</label>
-                          <textarea class="text-input" name="descricao" style="width:478px; height: 100px"><?php 
-                                   if(isset($dietaRetornada)) echo $dietaRetornada->getDescricao() 
-                           ?></textarea>
-                        </li>
-
+                          <label>CPF</label>
+                          <input type="text" name="cpf" value="<?php 
+                        if(isset($nutricionistaSelecionada)) echo $nutricionistaSelecionada->getCpf() ?>" class="text-input" style="white: 300px">
+                    </li>
+                    
+                    <li class="form-row text-input-row">
+                          <label>Endereço</label>
+                          <input type="text" name="endereco" value="<?php 
+                        if(isset($nutricionistaSelecionada)) echo $nutricionistaSelecionada->getEndereco() ?>" class="text-input" style="white: 300px">
+                    </li>
                         
+                     <li class="form-row text-input-row">
+                          <label>Telefone</label>
+                          <input type="text" name="telefone" value="<?php 
+                        if(isset($nutricionistaSelecionada)) echo $nutricionistaSelecionada->getTelefone() ?>" class="text-input" style="white: 300px">
+                    </li>
+                    
+                     <li class="form-row text-input-row">
+                          <label>E-mail</label>
+                          <input type="text" name="email" value="<?php 
+                        if(isset($nutricionistaSelecionada)) echo $nutricionistaSelecionada->getEmail() ?>" class="text-input" style="white: 300px">
+                    </li>
+                    
+                     <li class="form-row text-input-row">
+                          <label>CRN</label>
+                          <input type="text" name="crn" value="<?php 
+                        if(isset($nutricionistaSelecionada)) echo $nutricionistaSelecionada->getCrn() ?>" class="text-input" style="white: 300px">
+                    </li>           
 
-                        <li class="form-row text-input-row">
-                         <label>Alimentos</label>   
-                            <table style="width:500px; margin-left:100px">
-                               <tr>
-                                 <th>Descrição</th> 
-                                 <th>Cal. (%)</th>
-                                 <th>Carb. (%)</th>
-                                 <th>Prot. (%)</th>
-                                 <th>Gord. (%)</th>
-                                 <th>Sel.</th>
-                                </tr>
-
-                                 <?php foreach ($listaAlimentosSelecionados as $alimentoSelecionado) { ?>
-                                    <tr>
-                                        <td> <?php echo $alimentoSelecionado->getDescricao() ?> </td>
-                                        <td> <?php echo $alimentoSelecionado->getCaloria() ?> </td>
-                                        <td> <?php echo $alimentoSelecionado->getCarboidrato() ?> </td>
-                                        <td> <?php echo $alimentoSelecionado->getProteina() ?> </td>
-                                        <td> <?php echo $alimentoSelecionado->getGordura() ?> </td>
-                                        <td> <input type="checkbox" name="alimento<?php echo $alimentoSelecionado->getIdAlimento() ?>" value="true" checked> </td>
-                                    </tr>
-                                 <?php } 
-                                    
-                                     foreach($listaAlimentosNaoSelecionados as $alimentoNaoSelecionado)
-                                     { ?>
-                                        <tr>
-                                            <td> <?php echo $alimentoNaoSelecionado->getDescricao() ?> </td>
-                                            <td> <?php echo $alimentoNaoSelecionado->getCaloria() ?> </td>
-                                            <td> <?php echo $alimentoNaoSelecionado->getCarboidrato() ?> </td>
-                                            <td> <?php echo $alimentoNaoSelecionado->getProteina() ?> </td>
-                                            <td> <?php echo $alimentoNaoSelecionado->getGordura() ?> </td>
-                                        <td> <input type="checkbox" name="alimento<?php echo $alimentoNaoSelecionado->getIdAlimento() ?>" value="true"> </td>
-                                        </tr>
-                                    <?php
-                                     }
-                                 ?>
-
-
-                         </table>
-                        </li>
-
-
-
-                        <li class="button-row" style="margin-top:50px">
-                          <input type="submit" value="Salvar Alterações" name="submit" class="btn-submit">
-                        </li>
+                   <li class="button-row" style="margin-top: 50px">
+                        <input type="hidden" name="idNutricionista" value="<?php echo $_POST['idNutricionista'] ?>">
+                        <input type="submit" name="submit" value="Confirmar" class="btn-submit">
+                    </li> 
                       </ol>
-
-                    </fieldset>
-                  </form>
-                  <div class="response"></div>
-                </div>
+            </fieldset>
+        </form>
+    </div>
                 
              <?php
          }
-         else
-         {
-            if($_POST['submit']=='Salvar Alterações')
-            {
-                $listaAlimentosPrescritos = array();
-      
-                foreach(array_keys($_POST) as $parametro)
-                {
-                    if(strpos($parametro,'alimento') !== false)
-                    {
-                        $idAlimento = explode("alimento", $parametro);
-
-                        foreach($listaAlimentos as $alimento) 
-                        {
-                             if ($idAlimento[1] == $alimento->getIdAlimento()) 
-                             {  
-                                 array_push($listaAlimentosPrescritos, $alimento);
-                                 break;
-                             }
-                        }
-                    }
-                }
-
-
-                $dietaAlterada = new Dieta($_SESSION['dietaRetornada']->getIdDieta(), $_POST['descricao'], $listaAlimentosPrescritos, $_SESSION['Nutricionista'], $_SESSION['dietaRetornada']->getAluno());
-                
+        else
+     {
+          $nutricionista = new Nutricionista($_POST['idNutricionista'], 
+                             $_POST['nome'], 
+                             $_POST['cpf'],
+                             $_POST['endereco'],
+                             $_POST['telefone'],
+                             $_POST['email'],
+                             $_POST['crn'],
+                             $_SESSION['Coordenador']);
+    
                 try
                 {
-                    $fachada->alterarDieta($dietaAlterada);
-                    $mensagem = "A dieta do aluno ".$_SESSION['dietaRetornada']->getAluno()->getNome()." foi alterada com successo";
+                    $fachada->alterarNutricionista($nutricionista);
+                    $mensagem = "O Registro foi alterado com Sucesso!";
                 } 
                 catch (Exception $exc)
                 {
                     $mensagem = $exc->getMessage();
                 }
-
-            }
-            else
-            {
-                $mensagem = "Não foi selecionada a dieta";
-            }
-         }
-         
+     }
+                    
+    }
     }
     
-    if($camposPreenchidos && !isset($_POST['idDieta']) || isset($dietaAlterada)) { ?>
+    if(isset($_POST['submit']) && $_POST['submit']=="Confirmar"){ ?>
     
         <h3>Mensagem</h3>
         <p><?php echo $mensagem ?></p>
