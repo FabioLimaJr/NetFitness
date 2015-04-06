661550341397a8fca8a3f86e639ee94a6a651985
Erick Ferreira <efsgarbage@gmail.com>
2015-04-05 22:45:38 -0300

    aluno visualizar Dietas

---
+++ web/componentes/menuAluno.php
@@ -0,0 +1,16 @@
+<div id="menu" class="menu-v">
+      <ul>
+        <li><a href="index.php" class="active">Home</a>
+        </li>
+        <li><a href="#">Dietas</a>
+          <ul>
+            <li><a href="alunoListarDietas.php">Listar</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Treinos</a>
+          <ul>
+            <li><a href="alunoListarTreinos.php">Listar</a></li>
+          </ul>
+        </li>      
+      </ul>
+    </div>
