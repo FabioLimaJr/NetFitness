99ebd71c998f9c69035e786577d163ab78f906bc
Erick Ferreira <efsgarbage@gmail.com>
2015-04-27 23:31:35 -0300

    add listarDietaAlunoConteudo

---
+++ web/conteudo/listarDietasAlunoConteudo.php
@@ -0,0 +1,73 @@
+<?php 
+
+$fachada = new Fachada();
+$fachada = Fachada::getInstance();
+$mensagem ="";
+
+try
+{
+    $listaDietas = $fachada->listarDietas($_SESSION['Aluno'], EAGER);
+    //var_dump($listaDietas);
+} 
+catch (Exception $exc)
+{
+    $mensagem = $exc->getMessage();
+}   
+    
+    
+   
+ 
+    
+?>
+
+<h1 class="title">Listar Dietas</h1>
+    <div class="line"></div>
+    Telefone:<?php echo $aluno->getTelefone() ?> | Email:<?php echo $aluno->getEmail() ?> | Endereço:<?php echo $aluno->getEndereco() ?>
+    <div class="intro" style="margin-bottom:50px"></div>
+    
+    
+    
+    <h3>Usuário logado: <?php echo $aluno->getNome() ?></h3>
+   
+    <div class="clear"></div>
+    <div class="line"></div>
+
+    <div style="margin-bottom: 50px">
+        
+        <table style="width:100%">
+                    <tr>
+                      <th>Nome Aluno</th> 
+                      <th>Descrição Dieta</th>
+                      <th>Alimentos</th>
+                    </tr>
+                    
+                    <?php foreach ($listaDietas as $dieta){ ?>
+                    <tr>
+                        <td><?php echo $dieta->getAluno()->getNome() ?></td> 
+                        <td><?php echo $dieta->getDescricao() ?></td>  
+                        <td><?php 
+                                 $sizeListaAlimentos = count($dieta->getListaAlimentos());
+                                 $count = 1;
+                                 foreach ($dieta->getListaAlimentos() as $alimento)
+                                 {
+                                     if($count == $sizeListaAlimentos)
+                                     {
+                                       echo $alimento->getDescricao();
+                                     }
+                                     else
+                                     {
+                                       echo $alimento->getDescricao().", ";  
+                                     }
+                                     
+                                     $count++;
+                                 }
+                             ?></td>  
+                    </tr>
+                  
+                    <?php } ?>
+                    
+        </table>
+    </div>
+
+    
+    <?php include('componentes/footerOne.php') ?>
