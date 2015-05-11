9b4654b89d9f0f2eece208ca2b6341fb054a7c11
Erick Ferreira <efsgarbage@gmail.com>
2015-05-11 12:27:46 -0300

    ajustes nas telas  para atender os links do menu.

---
+++ web/pageParceiros.php
@@ -0,0 +1,28 @@
+<?php include ('componentes/header.php') ?>
+
+
+<body>
+<div id="wrapper">
+    
+  <div id="sidebar">
+    
+    <div id="logo"><a href="index.php"><img src="images/logo.png" alt=""></a></div>
+
+    <?php include ('componentes/menu.php') ?>  
+    <?php include ('componentes/leftIcons.php') ?>
+    <?php include ('componentes/signature.php'); ?>   
+    
+  </div><!-- end sidebar -->
+     
+  <div id="content">
+  
+     <?php include('conteudo/pageParceirosConteudo.php'); ?>
+    
+  </div><!-- end content -->
+  
+</div><!-- end wrapper -->
+    
+<?php include ('componentes/clear.php'); ?> 
+
+</body>
+</html>
