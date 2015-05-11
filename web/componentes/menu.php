7df7b6b72b83cabf2479496abd15920e5b54aa1e
Erick Ferreira <efsgarbage@gmail.com>
2015-05-11 12:24:32 -0300

    Ajustes menu principal

--- web/componentes/menu.php
+++ web/componentes/menu.php
@@ -1,36 +1,27 @@
     <div id="menu" class="menu-v">
       <ul>
         <li><a href="index.php" class="active">Home</a>
+        </li>
+        <li><a href="pageInstitucional.php">A Academia</a>
           <ul>
-            <li><a href="index.php">Home w/ Carousel</a></li>
-            <li><a href="index2.html">Home w/ Portfolio</a></li>
-            <li><a href="index3.html">Home w/ Testimonials</a></li>
+             <li><a href="pageInstitucional.php">Institucional</a></li>
+            <li><a href="pageParceiros.php">Parceiros</a></li>
+            <li><a href="pageInfraestrutura.php">Infraestrutura</a></li>
+            <li><a href="pageClubes.php">Clubes</a></li>
           </ul>
         </li>
-        <li><a href="portfolio.html">Portfolio</a>
+        <li><a href="pageMusculacao.php">Atividades</a>
           <ul>
-            <li><a href="portfolio.html">Portfolio 4 Columns</a></li>
-            <li><a href="portfolio2.html">Portfolio 3 Columns</a></li>
-            <li><a href="portfolio3.html">Portfolio 2 Columns</a></li>
-            <li><a href="portfolio4.html">Portfolio 1 Column</a></li>
-            <li><a href="portfolio-post.html">Portfolio Post</a></li>
+            <li><a href="pageMusculacao.php">Musculação</a></li>
+            <li><a href="pageErgometria.php">Ergometria</a></li>
+            <li><a href="pageGinastica.php">Ginastica</a></li>
+            <li><a href="pageAquatica.php">Aquatica</a></li>
           </ul>
         </li>
-        <li><a href="blog.html">Blog</a>
-          <ul>
-            <li><a href="blog.html">Blog</a></li>
-            <li><a href="blog2.html">Blog w/ Sidebar</a></li>
-            <li><a href="post.html">Post</a></li>
-            <li><a href="post2.html">Post w/ Sidebar</a></li>
-          </ul>
-        </li>
-        <li><a href="buttons-boxes-images.html">Features</a>
-          <ul>
-            <li><a href="buttons-boxes-images.html">Buttons Boxes Images</a></li>
-            <li><a href="tabs-toggle-table.html">Tabs Toggle Tables</a></li>
-            <li><a href="testimonials.html">Testimonials</a></li>
-            <li><a href="typography.html">Typography</a></li>
-            <li><a href="service-icons.html">Service Icons</a></li>
+        <li><a href="pageLocalozacao.php">Contatos e Localização</a>
+        <ul>
+            <li><a href="pageLocalozacao.php">Localização</a></li>
+            <li><a href="pageContato.php">Entre em contato</a></li>
           </ul>
         </li>
         <li><a href="login.php">Login</a></li>
