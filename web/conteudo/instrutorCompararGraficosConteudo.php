9b653b1db1e80bf39e020c43a3a034a6553b4c26
karter76 <karter76@gmail.com>
2015-05-21 18:36:07 +0200

    Adicionados os gráficos no menu Instrutor

--- web/conteudo/instrutorCompararGraficosConteudo.php
+++ web/conteudo/instrutorCompararGraficosConteudo.php
@@ -1,88 +1,162 @@
 <?php 
 
+$camposPreenchidos = false;
+$mensagem = "";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     $camposPreenchidos = true;
 }
- else
-{
-  $camposPreenchidos = false;    
-}
 
-$mensagem = "";
- 
-    
+if(!$camposPreenchidos)
+{
+    $fachada = Fachada::getInstance();
+    try
+    {
+        $listaExamesFisicos = $fachada->listarExamesFisicos($_SESSION['Instrutor'], EAGER);
+        $listaAlunos = array();
+
+        foreach($listaExamesFisicos as $exameFisico)
+        {
+            if( !in_array($exameFisico->getAluno(), $listaAlunos) )
+            {
+                array_push($listaAlunos, $exameFisico->getAluno());
+            }
+        }
+    }
+    catch (Exception $ex)
+    {
+         $mensagem = $ex->getMessage();
+    }
+}
 ?>
 
-<h1 class="title">Comparar Gráficos Exames Físicos</h1>
+<h1 class="title">Gráficos Exames Físicos</h1>
     <div class="line"></div>
-    Telefone:<?php echo $aluno->getTelefone() ?> | Email:<?php echo $aluno->getEmail() ?> | Endereço:<?php echo $aluno->getEndereco() ?>
+    Telefone:<?php echo $instrutor->getTelefone() ?> | Email:<?php echo $instrutor->getEmail() ?> | Endereço:<?php echo $instrutor->getEndereco() ?>
     <div class="intro" style="margin-bottom:50px"></div>
     
     
     
-    <h3>Usuário logado: <?php echo $aluno->getNome() ?></h3>
+    <h3>Usuário logado: <?php echo $instrutor->getNome() ?></h3>
    
     <div class="clear"></div>
     <div class="line"></div>
+    
+    <?php 
+    
+    if(!$camposPreenchidos)
+    { ?>
 
-    <?php if(!$camposPreenchidos) { ?>
-     <label>Selecionar os gráficos a comparar:</label>
-    <div style="margin-bottom: 30px; margin-top:30px">
-        <form class="forms" enctype="multipart/form-data" action="alunoCompararGraficos.php" method="post" >
-            <fieldset style="margin-left:50px;">
-           <ol>
-               
+    
+        <div class="form-container" style="margin-bottom:50px">
+            <form class="forms" action="instrutorCompararGraficos.php" method="post" >
+                <fieldset>
+                    <ol
+                        <li class="form-row text-input-row">
+                        <label>Alunos</label>   
+                            <table style="width:500px; margin-left:100px;margin-top:20px">
+                                <tr>
+                                  <th>Nome</th> 
+                                  <th>Selecione</th>
+                                </tr>
 
-               <li class="form-row checkbox-row">
-                
-                  <input type="checkbox" name="peso" value="ok" checked>Peso
-              </li>  
-              
-              <li class="form-row checkbox-row">
-                 <input type="checkbox" name="imc" value="ok">IMC
-              </li>   
-                <li class="form-row checkbox-row"> 
-                 <input type="checkbox" name="torax" value="ok">Circunferência Torax
-               </li>  
-               
-                <li class="form-row checkbox-row"> 
-                 <input type="checkbox" name="abdomen" value="ok">Circunferência Abdomen
-               </li>  
-               
-                <li class="form-row checkbox-row"> 
-                 <input type="checkbox" name="braco" value="ok">Circunferência Braço
-               </li>  
-               
-               <li class="form-row checkbox-row">
-                 <input type="checkbox" name="antebraco" value="ok">Circunferência Antebraço
-               </li>
-               
-                <li class="form-row checkbox-row"> 
-                 <input type="checkbox" name="coxa" value="ok">Circunferência Coxa
-               </li>
-               
-                <li class="form-row checkbox-row"> 
-                 <input type="checkbox" name="panturrilha" value="ok">Circunferência Panturrilha
-                </li> 
-           
-             
-              <li class="button-row" style="margin-top:50px">
-                  <input type="submit" value="Comparar" name="submit" class="btn-submit" style="margin-left:0">
-             </li>
-             
-           </ol>
+                                <?php foreach ($listaAlunos as $aluno) { ?>
+                                    <tr>
+                                        <td> <?php echo $aluno->getNome() ?> </td>
+                                        <td> <input type="radio" name="aluno<?php echo $aluno->getIdAluno() ?>" value="true" style="width: 30px"> </td>                                   
+                                    </tr>
+                                <?php } ?>
 
-         </fieldset>
-       </form>
-       <div class="response"></div>
-    </div>
-         
-     <?php 
-     }
-     else
-     {
+                             </table>
+                        </li>
+
+                        <li class="button-row" style="margin-top:50px">
+                            <input type="submit" value="Selecionar Aluno" name="submit" class="btn-submit">
+                        </li>
+                    </ol>
+                </fieldset>
+            </form>
+            <div class="response"></div>
+        </div>
+    
+    <?php 
+    }    
+    else
+    { 
+        if($_POST['submit']!="Comparar")
+        {
+            $fachada = Fachada::getInstance();
+            $mensagem ="";
+
+            foreach(array_keys($_POST) as $parametro)
+            {                    
+                if(strpos($parametro,'aluno') !== false)
+                {
+                    $idAluno = explode("aluno", $parametro);
+                    $aluno = new Aluno($idAluno[1]);
+                    $aluno = $fachada->detalharAluno($aluno, LAZY);
+                    break;
+                }
+            }
+            
+            $_SESSION['Aluno']= $aluno;
+        ?> 
+            Aluno: <?php echo $aluno->getNome() ?><br/><br/>
+            <label>Selecionar os gráficos a comparar:</label>
+            <div style="margin-bottom: 30px; margin-top:30px">
+                <form class="forms" enctype="multipart/form-data" action="instrutorCompararGraficos.php" method="post" >
+                    <fieldset style="margin-left:50px;">
+                   <ol>
+
+
+                       <li class="form-row checkbox-row">
+
+                          <input type="checkbox" name="peso" value="ok" checked>Peso
+                      </li>  
+
+                      <li class="form-row checkbox-row">
+                         <input type="checkbox" name="imc" value="ok">IMC
+                      </li>   
+                        <li class="form-row checkbox-row"> 
+                         <input type="checkbox" name="torax" value="ok">Circunferência Torax
+                       </li>  
+
+                        <li class="form-row checkbox-row"> 
+                         <input type="checkbox" name="abdomen" value="ok">Circunferência Abdomen
+                       </li>  
+
+                        <li class="form-row checkbox-row"> 
+                         <input type="checkbox" name="braco" value="ok">Circunferência Braço
+                       </li>  
+
+                       <li class="form-row checkbox-row">
+                         <input type="checkbox" name="antebraco" value="ok">Circunferência Antebraço
+                       </li>
+
+                        <li class="form-row checkbox-row"> 
+                         <input type="checkbox" name="coxa" value="ok">Circunferência Coxa
+                       </li>
+
+                        <li class="form-row checkbox-row"> 
+                         <input type="checkbox" name="panturrilha" value="ok">Circunferência Panturrilha
+                        </li> 
+
+
+                      <li class="button-row" style="margin-top:50px">
+                          <input type="submit" value="Comparar" name="submit" class="btn-submit" style="margin-left:0">
+                     </li>
+
+                   </ol>
+
+                 </fieldset>
+               </form>
+               <div class="response"></div>
+            </div>
+        <?php
+        }
+        else
+        {
         
         $fachada = Fachada::getInstance();
         $mensagem ="";
@@ -191,6 +265,7 @@ $mensagem = "";
                             array_push($legendas, "Panturrilha");
                         }
                         ?>
+                        Aluno: <?php echo $_SESSION['Aluno']->getNome() ?>
                         <div style="margin-bottom: 50px">
                             <image style="margin-bottom:50px;" src='../ferramentas/grafica/grafico.php?titulo=Comparar Graficos&largura=730&altura=350&dados=<?php echo serialize($matrixDados) ?>&legendas=<?php echo serialize($legendas)?>&comp=true'/>
 
@@ -219,12 +294,13 @@ $mensagem = "";
          
         
        }
-     
-     if($mensagem!="") { ?>
+    }
+    
+    if($mensagem!="") { ?>
     
         <h3>Mensagem</h3>
         <p><?php echo $mensagem ?></p>
     
     <?php }
-     
+    
     include('componentes/footerOne.php') ?>
