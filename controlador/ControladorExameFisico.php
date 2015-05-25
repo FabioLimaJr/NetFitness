b848cd29afe28ac90fd7c1b6105f558af4f94e94
Erick Ferreira <efsgarbage@gmail.com>
2015-05-25 19:58:00 -0300

    controlador para Controlador

---
+++ controlador/ControladorExameFisico.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Description of controladorExameFisico
+ *
+ * @author Daniele
+ */
+
+include ($serverPath.'repositorio/RepositorioExameFisico.php');
+
+
+class ControladorExameFisico
+{
+    private $repositorioExameFisico;
+    
+    public function __construct() 
+    {
+        $this->setRepositorioExameFisico(new RepositorioExameFisico());
+    }
+
+    
+    function getRepositorioExameFisico()
+    {
+        return $this->repositorioExameFisico;
+    }
+
+    function setRepositorioExameFisico($repositorioExameFisico)
+    {
+        $this->repositorioExameFisico = $repositorioExameFisico;
+    }
+    
+    public function inserir($exameFisico)
+    {
+        if(ExpressoesRegulares::validarExameFisico($exameFisico))
+        {
+            $this->getRepositorioExameFisico()->inserir($exameFisico);
+        }
+    }
+    
+    public function listar($pessoa, $fetchType)
+    {
+        //conferir obejto
+        return $this->getRepositorioExameFisico()->listar($pessoa, $fetchType);
+    }
+   
+    public function detalhar($exameFisico, $fetchType)
+    {
+        //conferir obejto e fetchType
+        return $this->getRepositorioExameFisico()->detalhar($exameFisico, $fetchType);
+    }
+    public function alterar($exameFisico)
+    {
+        //conferir obejto e fetchType
+        return $this->getRepositorioExameFisico()->lterar($exameFisico);
+    }
+
+}
