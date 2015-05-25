208f420f4d86bab593ce6be125381f1e56dfb7d3
Erick Ferreira <efsgarbage@gmail.com>
2015-05-25 20:38:20 -0300

    conteudo das paginas

--- web/conteudo/loginRespostaConteudo.php
+++ web/conteudo/loginRespostaConteudo.php
@@ -59,13 +59,7 @@
     
     
     
-    <h3>Malesuada Condimentum Inceptos Vehicula</h3>
-    <p>Sed posuere consectetur est at lobortis. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-    <ul>
-      <li>Sed posuere consectetur est at lobortis, Nullam id dolor id nibh ultricies vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-      <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
-      <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-    </ul>
+   
     
     
     <?php include('componentes/footerOne.php') ?>
